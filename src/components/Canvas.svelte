b75f3884ed25a07dbd0d3fa9dfd019fe2d13b852
devvdiego <83733071+DevvDiego@users.noreply.github.com>
2024-01-06 18:07:28 -0600

    Initialize Canvassvelte component

---
+++ src/components/Canvas.svelte
@@ -0,0 +1,17 @@
+<script>
+    export let _class = "";
+    export let w = "180px";
+    export let h = "200px";
+
+</script>
+
+<canvas width={w} height={h}
+class=" {_class} 
+        border-2 border-secondary-700"
+>
+
+</canvas>
+
+<style>
+
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,15 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+    import Canvas from "../components/Canvas.svelte";
+</script>
+
+<main>
+    <h1 class="text-2xl text-text-100">SketchIt app</h1>
+
+    <Canvas w={"500px"} h={"300px"}  />
+
+
+</main>
+
+
+<style>
+</style>
