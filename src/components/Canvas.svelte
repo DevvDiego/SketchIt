69d86084c90cb6f8be3ed3ec6c35663586bab96a
devvdiego <83733071+DevvDiego@users.noreply.github.com>
2024-01-06 18:11:17 -0600

    Set event listeners

--- src/components/Canvas.svelte
+++ src/components/Canvas.svelte
@@ -3,11 +3,17 @@
     export let w = "180px";
     export let h = "200px";
 
+    function test(){return 0;}
+
 </script>
 
-<canvas width={w} height={h}
+<canvas width={w} height={h} 
 class=" {_class} 
-        border-2 border-secondary-700"
+        border-2 border-secondary-700
+        "
+
+on:mousedown={test} on:mousemove={test}
+on:pointerdown={test} on:pointermove={test}
 >
 
 </canvas>
