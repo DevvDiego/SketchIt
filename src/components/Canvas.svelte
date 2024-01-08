7d0620200f62199f77e256935c172609232d8b86
devvdiego <83733071+DevvDiego@users.noreply.github.com>
2024-01-07 20:58:19 -0600

    set basic canvas functionality

--- src/components/Canvas.svelte
+++ src/components/Canvas.svelte
@@ -1,20 +1,53 @@
 <script>
+    import { onMount } from "svelte";
+    
+    /**
+     * use this element only inside onMount() 
+    */
     export let _class = "";
     export let w = "180px";
     export let h = "200px";
+    
+    let root;
+    let ctx;
 
-    function test(){return 0;}
+    let fillStyle = "white";
+    let draw_color = "black";
+    let draw_width = "2"
+    let is_drawing = false;
+
+
+    onMount(()=>{
+        ctx = root.getContext("2d");
+        ctx.fillStyle = fillStyle;
+        ctx.fillRect(0, 0, root.width, root.height)
+
+
+
+    })
+
+
+    function move(){
+        console.log("move")
+        
+    }
+
+    function draw(){
+
+    }
+
+    
 
 </script>
 
-<canvas width={w} height={h} 
+<canvas bind:this={root} width={w} height={h} 
 class=" {_class} 
         border-2 border-secondary-700
         "
-
-on:mousedown={test} on:mousemove={test}
-on:pointerdown={test} on:pointermove={test}
->
+        
+on:mousedown={move} on:mousemove={move}
+on:pointerdown={move} on:pointermove={move}
+> 
 
 </canvas>
 
